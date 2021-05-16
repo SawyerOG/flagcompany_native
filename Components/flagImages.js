6b37a7a4f3a4df67c1f84bff323488373c9e55e3
SawyerOG <oglesbysaw@gmail.com>
2021-05-16 15:58:39 -0600

    Generate flag image

---
+++ Components/flagImages.js
@@ -0,0 +1,29 @@
+/* global require */
+
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+
+const FLAG = ({ type }) => {
+    let image;
+    switch (type) {
+        case 'US':
+            image = <Image style={s.image} source={require('../assets/us.png')} height={30} width={30} />;
+            break;
+        case 'Colorado':
+            image = <Image style={s.image} source={require('../assets/co.png')} height={30} width={30} />;
+            break;
+        default:
+            image = <Image style={s.image} source={require('../assets/blankFlag.png')} height={30} width={30} />;
+    }
+
+    return image;
+};
+
+export default FLAG;
+
+const s = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 60,
+    },
+});
